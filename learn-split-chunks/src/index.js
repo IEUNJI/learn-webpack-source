fb82607d688cc0473a0ef9a0faa47adb99756605
linzijun <linzjk@yonyou.com>
2021-05-20 16:50:26 +0800

    webpack实现 异步

--- learn-split-chunks/src/index.js
+++ learn-split-chunks/src/index.js
@@ -1,3 +1,13 @@
-const hello = require('./hello.js');
+console.log('index');
 
-console.log(hello);
+const btn = document.createElement('button');
+
+btn.innerHTML = 'click';
+
+btn.addEventListener('click', () => {
+  import('./hello.js').then(module => {
+    console.log(module.default);
+  });
+});
+
+document.body.appendChild(btn);
--- learn-split-chunks/webpack.js
+++ learn-split-chunks/webpack.js
@@ -9,6 +9,7 @@ const generator = require('babel-generator').default;
 const ejs = require('ejs');
 
 const mainTemplate = fs.readFileSync('./main.ejs', 'utf8');
+const chunkTemplate = fs.readFileSync('./chunk.ejs', 'utf8');
 
 class Compiler {
   constructor(config) {
@@ -19,23 +20,29 @@ class Compiler {
     const { entry } = this.config;
 
     this.entry = entry;
-    this.modules = {};
+    this.chunks = {
+      main: {}
+    };
 
-    this.buildModule(entry);
+    this.buildModule(entry, 'main');
     this.emitFiles();
 
     callback();
   }
 
-  buildModule(moduleId) {
+  buildModule(moduleId, chunkId) {
     const originalSource = fs.readFileSync(moduleId, 'utf8');
 
-    const ast = babylon.parse(originalSource);
+    const ast = babylon.parse(originalSource, {
+      plugins: ['dynamicImport']
+    });
 
     const dependencies = [];
 
     traverse(ast, {
-      CallExpression({ node }) {
+      CallExpression: (nodePath) => {
+        const { node } = nodePath;
+
         if (node.callee.name === 'require') {
           node.callee.name = '__webpack_require__';
 
@@ -44,13 +51,26 @@ class Compiler {
 
           dependencies.push(dependencyModuleId);
           node.arguments = [types.stringLiteral(dependencyModuleId)];
+        } else if (types.isImport(node.callee)) {
+          const moduleName = node.arguments[0].value;
+          const dependencyModuleId = './' + path.posix.join(path.posix.dirname(moduleId), moduleName);
+          const dependencyChunkId = dependencyModuleId.slice(2).replace(/\/|\./g, '_') + '.js';
+
+          nodePath.replaceWithSourceString(`
+            __webpack_require__.e("${dependencyChunkId}").then(
+              __webpack_require__.t.bind(__webpack_require__, "${dependencyModuleId}")
+            )
+          `);
+
+          this.buildModule(dependencyModuleId, dependencyChunkId);
         }
       }
     });
 
     const { code } = generator(ast);
 
-    this.modules[moduleId] = code;
+    this.chunks[chunkId] = this.chunks[chunkId] || {};
+    this.chunks[chunkId][moduleId] = code;
 
     dependencies.forEach(dependency => {
       this.buildModule(dependency);
@@ -60,14 +80,27 @@ class Compiler {
   emitFiles() {
     const { output } = this.config;
 
-    const outputFile = path.posix.join(output.path, output.filename);
+    Object.keys(this.chunks).forEach(chunkId => {
+      if (chunkId === 'main') {
+        const outputFile = path.posix.join(output.path, output.filename);
 
-    const bundle = ejs.render(mainTemplate, {
-      entry: this.entry,
-      modules: this.modules
+        const bundle = ejs.render(mainTemplate, {
+          entry: this.entry,
+          modules: this.chunks[chunkId]
+        });
+
+        fs.writeFileSync(outputFile, bundle);
+      } else {
+        const outputFile = path.posix.join(output.path, chunkId);
+
+        const bundle = ejs.render(chunkTemplate, {
+          chunkId,
+          modules: this.chunks[chunkId]
+        });
+
+        fs.writeFileSync(outputFile, bundle);
+      }
     });
-
-    fs.writeFileSync(outputFile, bundle);
   }
 }
 
